c1b25c27b68e821c63057ac57abccabb87e6d20f
Kai Hooke <kaiorhu@gmail.com>
2018-10-20 01:03:11 -0400

    Update get_label_file.sh

--- datafile/get_label_file.sh
+++ datafile/get_label_file.sh
@@ -1,2 +1,2 @@
-wget https://www.kaggle.com/c/inclusive-images-challenge/download/train_human_labels.csv
-wget https://www.kaggle.com/c/inclusive-images-challenge/download/classes-trainable.csv
+curl http://iovip.qbox.me/train_human_labels.csv -H 'Host:p5k8s41b2.bkt.clouddn.com' -o train_human_labels.csv
+curl http://iovip.qbox.me/classes-trainable.csv -H 'Host:p5k8s41b2.bkt.clouddn.com' -o classes-trainable.csv
