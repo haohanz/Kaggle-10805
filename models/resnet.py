157611804fc9edf6cd12349384b1a552d70bce61
Kai Hooke <kaiorhu@gmail.com>
2018-10-21 15:16:25 -0400

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -57,7 +57,7 @@ class Bottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=7178):
+    def __init__(self, block, num_blocks, num_classes=7172):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
