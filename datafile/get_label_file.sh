65b7366e4fcf2157988c7a36bfc8c81f0bf049cb
Kai Hooke <kaiorhu@gmail.com>
2018-10-22 01:52:12 -0400

    Update get_label_file.sh

--- datafile/get_label_file.sh
+++ datafile/get_label_file.sh
@@ -1,2 +1,3 @@
 curl http://iovip.qbox.me/train_human_labels.csv -H 'Host:p5k8s41b2.bkt.clouddn.com' -o train_human_labels.csv
 curl http://iovip.qbox.me/classes-trainable.csv -H 'Host:p5k8s41b2.bkt.clouddn.com' -o classes-trainable.csv
+curl http://iovip.qbox.me/stage_1_sample_submission.csv -H 'Host:p5k8s41b2.bkt.clouddn.com' -o stage_1_sample_submission.csv
